cd0f3ae6414067d6a744f330bb5a84d0f84923d2
Ed Tansey <office.fwd@yahoo.com>
2021-12-02 23:41:54 +0000

    Design about and error pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+      	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel erat est. Vestibulum pulvinar ex ut nulla 
+      	consectetur varius. Nullam elementum cursus est, ac suscipit ex. Curabitur at sapien maximus, sagittis ligula sit 
+      	amet, placerat quam. Nullam sodales sapien non mauris hendrerit posuere. Aenean eget tortor lacus. Nullam ornare 
+      	maximus bibendum. Nunc dictum, est cursus convallis maximus, turpis sapien tempor felis, in aliquet sem ex id leo. 
+      	Vivamus viverra laoreet dolor ut commodo. Nulla ac lacus sem. Sed eu mollis lectus. Fusce nunc lectus, pulvinar ut 
+      	posuere et, tempus eget est. Nullam maximus felis non nunc mollis sodales. 
+      </p>
+    </section>
   )
 }
 
